e90e74bfdbe9b0cd7b8bb9e6019b11cb615d9007
MicaelZonta <micael.pinheiro@outlook.com.br>
2022-07-17 16:09:17 -0300

    Fix

--- src/main/kotlin/com/teste/core/port/in/TodoPort.kt
+++ src/main/kotlin/com/teste/core/port/in/TodoPort.kt
@@ -4,33 +4,26 @@ import com.teste.config.usercontext.UserContextHolder
 import com.teste.config.usercontext.UserContextModel
 import com.teste.core.port.`in`.transferobject.TodoRequest
 import com.teste.core.port.`in`.transferobject.TodoResponse
-import com.teste.core.port.out.Send
 import com.teste.infra.persistence.mysql.entity.TodoEntity
 import com.teste.infra.persistence.mysql.repository.TodoRepository
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
-import java.net.HttpURLConnection
-import java.net.URL
+
 
 @Controller("/todos")
 class TodoPort(
     private val todoRepository: TodoRepository,
     val userContextHandler: UserContextHolder,
-    val sendPort: Send
 ) {
 
     var userContextModel = UserContextModel()
 
     @Get
     fun getTodos(): HttpResponse<List<TodoResponse>> {
-
-        Thread.sleep(3000)
         println("CorrelationId: ${userContextHandler.getCorrelationId()}")
         println("TransactionId: ${userContextHandler.getTransactionId()}")
-
-        this.sendPort.fetchReleases()
 
         //Lista Persistence
         val todoEntityList = todoRepository.findAll()
