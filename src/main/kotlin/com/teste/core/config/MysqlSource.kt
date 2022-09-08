76712a1be6371af8432e1333ac597f648c089ae4
MicaelZonta <micael.pinheiro@outlook.com.br>
2022-09-08 14:01:56 -0300

    mysql

--- src/main/kotlin/com/teste/core/config/MysqlSource.kt
+++ src/main/kotlin/com/teste/core/config/MysqlSource.kt
@@ -1,4 +1,26 @@
 package com.teste.core.config
 
-class MysqlSource {
+import com.zaxxer.hikari.HikariConfig
+import io.micronaut.configuration.jdbc.hikari.HikariUrlDataSource
+import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Primary
+import jakarta.inject.Singleton
+import javax.sql.DataSource
+
+@Factory
+class MysqlSourceUrl{
+
+    @Singleton
+    @Primary
+    fun dataSource(): DataSource? {
+        val config = HikariConfig()
+        config.jdbcUrl = "jdbc:mysql://localhost:3307/testando"
+        config.username = "root"
+        config.password = "root"
+        config.driverClassName = "com.mysql.cj.jdbc.Driver"
+        config.connectionTestQuery = "SELECT 1"
+        return HikariUrlDataSource(config)
+
+    }
+
 }
